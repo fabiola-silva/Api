a1f6fcb3d7933476647580b6d37f009cb81331f1
Christian Reis <christianreisoffice@gmail.com>
2018-05-11 17:23:51 -0400

    adicão login online

--- model/config/config.php
+++ model/config/config.php
@@ -1,5 +1,8 @@
 <?php
 define('DB_HOST','localhost');
-define('DB_NAME','portfolio_db');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_NAME','lab31487_portfolio_db');
+//define('DB_NAME','portfolio_db');
+//define('DB_USER','root');
+define ('DB_USER','lab31487_fab');
+//define('DB_PASS','');
+define('DB_PASS','R{aPJ!m;O@TD');
